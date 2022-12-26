6e203011c19be949a455a08667eabe214214b4e0
danilo-hung <andyloveyoutoo2019@gmail.com>
2022-12-26 20:52:32 +0800

    create home page and style it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './cats.style.scss'
 
-function App() {
+const App = () => {
+  const cats = [
+    {
+      id: 1,
+      title: "Hats",
+      imgUrl: "https://images.unsplash.com/photo-1622445275576-721325763afe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600"
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imgUrl: "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600"
+
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imgUrl: "https://images.unsplash.com/photo-1618677831708-0e7fda3148b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600"
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imgUrl: "https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600"
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imgUrl: "https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=600"
+    },
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="cats-container" >
+      {cats.map(({ id, title, imgUrl }) => (
+        <div key={id} className='cat-body-box' >
+          <div className="cat-container" >
+            <div className='cat-line'> </div>
+            <div className="background-img">
+              <img src={imgUrl} alt="cat-img" />
+            </div>
+            <div className='content-box'>
+              <h2 className='title'>{title}</h2>
+              <h2 className='sub-title'>Shop Now</h2>
+            </div>
+          </div>
+        </div>
+      )
+      )}
     </div>
   );
 }
